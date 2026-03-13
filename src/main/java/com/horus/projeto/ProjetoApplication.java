72c87bfb9ab6f3f1b9dc7f04eef1d5f53ddb0fd1
kauanaraujo18 <fekajone2017@gmail.com>
2026-03-12 21:49:22 -0300

    Ajuste fuso horário

--- src/main/java/com/horus/projeto/ProjetoApplication.java
+++ src/main/java/com/horus/projeto/ProjetoApplication.java
@@ -3,12 +3,19 @@ package com.horus.projeto;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+import jakarta.annotation.PostConstruct;
+
 @SpringBootApplication
 public class ProjetoApplication {	
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProjetoApplication.class, args);
-		System.out.println("SENHA 123 CRIPTOGRAFADA: " + new org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder().encode("1212"));
 	}
+
+	@PostConstruct
+    public void init() {
+        TimeZone.setDefault(TimeZone.getTimeZone("America/Sao_Paulo"));
+    }
 
 }
